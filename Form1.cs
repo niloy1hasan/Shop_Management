e27ec16af833c8636258e5ade0dfa09db36bef75
niloy1hasan <notecse23@gmail.com>
2025-06-02 13:57:32 +0600

    dashboard added and menu added on form

--- Form1.cs
+++ Form1.cs
@@ -10,11 +10,41 @@ using System.Windows.Forms;
 
 namespace Shop_Management
 {
-    public partial class Form1 : Form
+    public partial class ShopManagement : Form
     {
-        public Form1()
+        public ShopManagement()
         {
             InitializeComponent();
+            LoadScreen(new DashboardScreen());
+        }
+
+
+
+        private void LoadScreen(UserControl screen)
+        {
+            mainContentPanel.Controls.Clear();
+            screen.Dock = DockStyle.Fill;
+            mainContentPanel.Controls.Add(screen);
+        }
+
+
+        private void minimizeButton_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+                "Are you sure you want to exit?",
+                "Confirm Exit",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Warning);
+
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
         }
     }
 }
